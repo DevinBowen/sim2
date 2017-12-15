162b7f889ed2a32c10fd0df9f6887fc872420ac0
devin <deivnobowen@gmail.com>
2017-12-15 16:51:59 -0700

    backend is finished

--- server/server.js
+++ server/server.js
@@ -4,17 +4,34 @@ const express = require('express')
     , bodyParser = require('body-parser')
     , cors = require('cors')
     , massive = require('massive')
+    , controller = require('./controller')
 
 
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+// app.use(express.static(`${__dirname}/../public/public`));
 const port = process.env.PORT
+
+app.get('/api/shelf/:id', controller.shelf);
+
+app.get('/api/bin/:id', controller.bin);
+
+app.put('/api/bin/:id', controller.updateBin);
+
+app.delete('api/bin/:id', controller.deleteBin);
+
+app.post('/api/bin/:id', controller.addBin);
+
+
+
 
 massive(process.env.CONNECTION_STRING).then(db => {
     app.set('db', db)
     app.get('db').init.seed_file().then(responce => {
         console.log(responce)
     })
-    app.listen(() => {
-        console.log(`wubulubadubdub on port ${port}.`)
-    })
 })
+        app.listen(port,() => {
+            console.log(`wubulubadubdub on port ${port}.`)
+        })
